7b81dc67c697491d2ba9d2daf8e12a2e6e5c2ec9
redim <redim@redim.com>
2024-11-22 08:43:16 +0700

    update sample and main

--- app/main.py
+++ app/main.py
@@ -96,6 +96,7 @@ async def upload_timekeeping(
         raise HTTPException(status_code=500, detail=str(e))
 
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
+    import uvicorn
 
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
